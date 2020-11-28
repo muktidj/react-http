c8659d7c759ae6d581f3fc72edef9b9e5a6788d1
muktidj <muktidwijatmoko@gmail.com>
2020-11-28 14:51:50 +0700

    bab 9, 170 Handling Errors Localy

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,11 +11,12 @@ class Blog extends Component {
 
     state = {
         posts:[],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
-        Axios.get('https://jsonplaceholder.typicode.com/posts')
+        Axios.get('https://jsonplaceholder.typicode.com/posts/')
         .then(response => {
             const posts = response.data.slice(0,4)
             const updatedPosts = posts.map(post => {
@@ -27,6 +28,9 @@ class Blog extends Component {
             this.setState({posts: updatedPosts})
             console.log(response)
         })
+        .catch(error => {
+            this.setState({error: true})
+        })
     }
 
     postSelectedHandler = (id) => {
@@ -35,16 +39,20 @@ class Blog extends Component {
 
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-                />
-            )
-        })
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                )
+            })
+
+        }
         return (
             <div>
                 <section className="Posts">
