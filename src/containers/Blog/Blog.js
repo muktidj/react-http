508d4e9db038580b9724889d51dfe3985d5af33b
muktidj <muktidwijatmoko@gmail.com>
2020-12-09 12:42:10 +0700

    bab 10, 211 Condiotal Rederect Request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
-import { Route, NavLink, Switch } from 'react-router-dom'
+
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 
 import './Blog.css';
@@ -38,9 +38,9 @@ class Blog extends Component {
                {/* <Route path="/" exact render={()=> <h1>Home</h1>}/>
                <Route path="/" render={()=> <h1>Home 2</h1>}/> */}
                <Switch>
-               <Route path="/new-post" component={NewPost} />
-               <Route path="/posts" component={Posts} />
-
+                <Route path="/new-post" component={NewPost} />
+                <Route path="/posts" component={Posts} />
+                <Redirect from="/" to="/posts" />
                </Switch>
 
             </div>
