b4e6ebeb8dff81a1cd2298afae1d5bbf49770b89
muktidj <muktidwijatmoko@gmail.com>
2020-12-11 14:06:09 +0700

    bab 11, 217 Routing and Server Deplyoment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog'
 class App extends Component {
   render() {
     return (
+      //<BrowserRouter basename="/my-app">, routing server development
       <BrowserRouter>
         <div className="App">
           <Blog />
